481be955b888f4d74055a698c8d4f4a421419a10
mmazzarolo <mazzarolomatteo@gmail.com>
2015-10-22 16:53:32 +0200

    Added login with JWT tokens

--- popup.js
+++ popup.js
@@ -1,24 +1,128 @@
 $(document).ready(function() {
-  $.getJSON("http://localhost:3000/users/mazza", function(data) {
-    $.each(data.user.bookmarks, function(key, val) {
-      // var list = $("#list");
-      // var li = $('<li></li>')
-      //   .appendTo(list);
-      // var img = $('<img>');
-      // img.attr('src', val.favicon)
-      // img.appendTo(li);
-      // var a = $('<a>');
-      // a.attr('href', val.url)
-      // a.text(val.name)
-      // a.onclick = function () {
-      //   alert('asdf');
-      //   chrome.tabs.create({active: true, url: val.url});
-      // }
-      // a.appendTo(li);
-      $("#list").append("<li><img src='" + val.favicon + "''><a href='" + val.url + "'>" + val.name + "</a> ");
-    });
+
+  console.log('Are you logged in? ' + isLoggedIn());
+
+  updateList();
+
+  refreshForms();
+
+  $('#loginForm').submit(function(event) {
+    login();
+    event.preventDefault();
+  });
+
+  $('#logoutForm').submit(function(event) {
+    logout();
+    event.preventDefault();
+  });
+
+  $('#addBookmarkForm').submit(function(event) {
+    addBookmark();
+    event.preventDefault();
   });
 });
+
+function refreshForms() {
+  if (isLoggedIn()) {
+    $('#loginForm').hide();
+    $('#logoutForm').show();
+    $('#addBookmarkForm').show();
+  } else {
+    $('#loginForm').show();
+    $('#logoutForm').hide();
+    $('#addBookmarkForm').hide();
+  }
+}
+
+function isLoggedIn() {
+  return (localStorage.getItem('token'));
+}
+
+function updateList() {
+  console.log('-> updateList');
+  if (isLoggedIn()) {
+
+    var authToken = 'Bearer ' + localStorage.getItem('token');
+
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3000/user',
+      beforeSend: function (request) {
+        request.setRequestHeader('authorization', authToken);
+      }
+    }).done(function(data) {
+      console.log('Done');
+      $.each(data.bookmarks, function(key, val) {
+        appendToList(val);
+      });
+    }).fail(function() {
+      console.log('Fail');
+    }).always(function(data) {
+      console.log(JSON.stringify(data));
+    });
+  }
+}
+
+function appendToList(bookmark) {
+  $("#list").append("<li><img src='" + bookmark.favicon + "''><a class='url' href='" + bookmark.url + "'>" + bookmark.name + "</a> ");
+}
+
+function login() {
+  console.log('-> login');
+  var formData = {
+    'email': $('input[name=email]').val(),
+    'password': $('input[name=password]').val(),
+  };
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/auth/login',
+    data: formData,
+    encode: true
+  }).done(function(data) {
+    console.log('Done');
+    localStorage.setItem('email', formData.email);
+    localStorage.setItem('token', data.token);
+    refreshForms();
+    updateList();
+  }).fail(function() {
+    console.log('Fail');
+  }).always(function(data) {
+    console.log(JSON.stringify(data));
+  });
+};
+
+function logout() {
+  console.log('-> logout');
+  localStorage.removeItem('email');
+  localStorage.removeItem('token');
+  refreshForms();
+};
+
+function addBookmark() {
+  console.log('-> addBookmark');
+  chrome.tabs.getSelected(null, function(tab) {
+    var bookmark = { url: tab.url };
+    var authToken = 'Bearer ' + localStorage.getItem('token');
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/user/bookmarks',
+      data: bookmark,
+      encode: true,
+      beforeSend: function (request) {
+        request.setRequestHeader('authorization', authToken);
+      }
+    }).done(function(data) {
+      console.log('Done');
+      appendToList(data);
+    }).fail(function() {
+      console.log('Fail');
+    }).always(function(data) {
+      console.log(JSON.stringify(data));
+    });
+
+  });
+};
 
 window.addEventListener('click', function(e) {
   if (e.target.href !== undefined) {
